965dd393d1c2b7f840fc8700d540435cc34d4e08
Miguel Robledo <mrobledo@lisyx.com>
2017-09-14 14:02:03 -0500

    Initial Commit

---
+++ LiferayScreensThemePepsico/LiferayScreensThemePepsico.h
@@ -0,0 +1,19 @@
+//
+//  LiferayScreensThemePepsico.h
+//  LiferayScreensThemePepsico
+//
+//  Created by MacBook Pro on 14/09/17.
+//  Copyright © 2017 lisyx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LiferayScreensThemePepsico.
+FOUNDATION_EXPORT double LiferayScreensThemePepsicoVersionNumber;
+
+//! Project version string for LiferayScreensThemePepsico.
+FOUNDATION_EXPORT const unsigned char LiferayScreensThemePepsicoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LiferayScreensThemePepsico/PublicHeader.h>
+
+
